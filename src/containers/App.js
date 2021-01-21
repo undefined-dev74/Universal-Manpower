ae77978fca25420276ee54fe1b5c2a95e669e0d3
JuniorBaymax <cooladitya953@gmail.com>
2021-01-21 23:35:00 +0530

    Added Navbar and Hero section componenet

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,19 @@
 import './App.scss';
+import React from 'react';
 import Header from '../components/Header/Header';
+import Hero from '../components/Hero/Hero';
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
       <Header />
-    </div>
+      {/* <Hero /> */}
+      {/* Header and Navbar section */}
+      {/* Hero Jumbotron section */}
+      {/* Job catogary section */}
+      {/* upload content or cv section  */}
+      {/* About company section */}
+      {/* footer section will goes here */}
+    </React.Fragment>
   );
 }
 
